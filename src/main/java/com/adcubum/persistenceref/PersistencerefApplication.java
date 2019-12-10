c5cdcb5c6042c18b557763caed138ff5cb5919d1
yury.yudin <yury.yudin@adcubum.com>
2019-12-10 11:15:55 +0100

    f Draft of the spring boot application with JPA in dependencies.

---
+++ src/main/java/com/adcubum/persistenceref/PersistencerefApplication.java
@@ -0,0 +1,13 @@
+package com.adcubum.persistenceref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistencerefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistencerefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adcubum/persistenceref/PersistencerefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adcubum.persistenceref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistencerefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
